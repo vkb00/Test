a6f79d4b0545eb0606ec445bd3202a0d1185e89b
vkb00 <vkb7662@gmail.com>
2018-04-07 15:54:01 +0300

    last version

---
+++ КурсоваяC(шарп) v1.0/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace КурсоваяC_шарп__v1._0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.справкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(33, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(37, 205);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 32);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Visible = false;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(408, 205);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 32);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать тест!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(32, 263);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите вариант ответа!";
+            this.label2.Visible = false;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem,
+            this.выходToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(637, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // справкаToolStripMenuItem
+            // 
+            this.справкаToolStripMenuItem.Name = "справкаToolStripMenuItem";
+            this.справкаToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.справкаToolStripMenuItem.Text = "Справка";
+            this.справкаToolStripMenuItem.Click += new System.EventHandler(this.справкаToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // button2
+            // 
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Image = global::КурсоваяC_шарп__v1._0.Properties.Resources._21;
+            this.button2.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button2.Location = new System.Drawing.Point(585, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 39);
+            this.button2.TabIndex = 5;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 318);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem справкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ КурсоваяC(шарп) v1.0/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace КурсоваяC_шарп__v1._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] num= new string[10];
+        string[] temp_ch_true = new string[10];
+        
+
+        int[]  temp_in_true = new int[10];
+       
+
+        public void Num()
+        {
+            num[0] = "quest1.txt";
+            num[1] = "quest2.txt";
+            num[2] = "quest3.txt";
+            num[3] = "quest4.txt";
+            num[4] = "quest5.txt";
+            num[5] = "quest6.txt";
+            num[6] = "quest7.txt";
+            num[7] = "quest8.txt";
+            num[8] = "quest9.txt";
+            num[9] = "quest10.txt";
+        }
+        public void Temp()
+        {
+            temp_ch_true[0] = "D";
+            temp_ch_true[1] = "A";
+            temp_ch_true[2] = "A";
+            temp_ch_true[3] = "B";
+            temp_ch_true[4] = "B";
+            temp_ch_true[5] = "C";
+            temp_ch_true[6] = "A";
+            temp_ch_true[7] = "B";
+            temp_ch_true[8] = "A";
+            temp_ch_true[9] = "D";
+
+            
+
+            for (int i=0; i < 10; i++)
+            {
+                temp_in_true[i] = i;
+               
+            }
+        }
+
+        int d=1;
+        int t = 0;
+        int h=0;//переменная положительных ответов
+        int g = 0;//переменная отрецательных ответов
+        int[] rand = new int[10] {0,0,0,0,0,0,0,0,0,0};
+        Random r = new Random();
+        string[] tempch = new string[10];
+        int[] tempin = new int[10];
+        
+            
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox1.Visible = true;
+            button1.Text = "Следующий!";
+           
+            if (t==0)
+            label2.Visible = true;
+
+            Num();
+            Temp();
+            if (t == 5)
+            {
+                tempin[t - 1] = rand[t - 1];
+                tempch[t - 1] = textBox1.Text;
+                
+            }
+            if (d<6)
+            {
+
+                
+
+                rand[t] = r.Next(10);
+
+                if (t==0)//условия чтобы рандом не повторялся
+                {
+                   
+                    FileStream MainFile = new FileStream(num[rand[t]], FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    StreamReader reader = new StreamReader(MainFile, Encoding.Default);
+                    label1.Text = reader.ReadToEnd();
+                    reader.Close();
+                    
+
+                }
+                if(t==1)
+                {
+                    while (rand[t] == rand[t - 1]) 
+                      rand[t] = r.Next(10);
+
+                    
+                    FileStream MainFile = new FileStream(num[rand[t]], FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    StreamReader reader = new StreamReader(MainFile, Encoding.Default);
+                    label1.Text = reader.ReadToEnd();
+                    reader.Close();
+
+                    tempin[t-1] = rand[t-1];
+                    tempch[t - 1] = textBox1.Text;
+                    
+                    
+
+
+                }
+                if (t == 2)
+                {
+                    while (rand[t] == rand[t - 1] || rand[t] == rand[t - 2])
+                        rand[t] = r.Next(10);
+
+                  
+                    FileStream MainFile = new FileStream(num[rand[t]], FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    StreamReader reader = new StreamReader(MainFile, Encoding.Default);
+                    label1.Text = reader.ReadToEnd();
+                    reader.Close();
+
+                    tempin[t - 1] = rand[t - 1];
+                    tempch[t - 1] = textBox1.Text;
+                    
+                }
+                if (t == 3)
+                {
+                    while (rand[t] == rand[t - 1] || rand[t] == rand[t - 2] || rand[t] == rand[t - 3])
+                        rand[t] = r.Next(10);
+
+                   
+                    FileStream MainFile = new FileStream(num[rand[t]], FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    StreamReader reader = new StreamReader(MainFile, Encoding.Default);
+                    label1.Text = reader.ReadToEnd();
+                    reader.Close();
+
+                    tempin[t - 1] = rand[t - 1];
+                    tempch[t - 1] = textBox1.Text;
+                    
+                }
+                if (t == 4)
+                {
+                    while (rand[t] == rand[t - 1] || rand[t] == rand[t - 2] || rand[t] == rand[t - 3] || rand[t] == rand[t - 4])
+                        rand[t] = r.Next(10);
+
+                    
+                    FileStream MainFile = new FileStream(num[rand[t]], FileMode.OpenOrCreate, FileAccess.ReadWrite);
+                    StreamReader reader = new StreamReader(MainFile, Encoding.Default);
+                    label1.Text = reader.ReadToEnd();
+                    reader.Close();
+
+                    tempin[t - 1] = rand[t - 1];
+                    tempch[t - 1] = textBox1.Text;
+                    
+                }
+                
+                
+                t++;
+            }
+            else
+            {
+                button1.Visible = false;
+                button2.Visible = true;
+                textBox1.Visible = false;
+                label2.Text = "";
+                label1.Text = " Ваши результаты:\n";
+                for (int i = 0; i < 5; i++)
+                {
+                    if (temp_ch_true[tempin[i]] == tempch[i])
+                    {
+                        label1.Text += (" Номер вопроса: " + Convert.ToString(tempin[i] + 1) + " Ваш ответ " + tempch[i] + "; Правильно!" + "\n");
+                        h++;
+                    }
+                    else
+                    {
+                        label1.Text += (" Номер вопроса: " + Convert.ToString(tempin[i] + 1) + " Ваш ответ " + tempch[i] + "; Неравильно!" + "\n");
+                        g++;
+                    }
+                }
+                if (h <= 2)
+                    label1.Text += (" \n Количество вопросов: 5\n Количество правильных ответов: " + h + "\n Ваша оценка: 2");
+                else
+                    label1.Text += ("\n Количество вопросов: 5\n Количество правильных ответов: " + h + "\n Ваша оценка: " + h);
+
+            }
+            d++;
+            textBox1.Text = "";
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length == 0)
+            {
+                label2.Visible = true;
+                button1.Enabled = false;
+
+            }
+            else
+            {
+                label2.Visible = false;
+                button1.Enabled = true;
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char l = e.KeyChar;
+            
+            if ((e.KeyChar < 65 || e.KeyChar > 68) && l != 8 )//запись в поле текст бокс только определённых букв с помощью кодировки ASCII
+                e.Handled = true;
+        }
+
+       
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void справкаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            this.Close();
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form3 f3 = new Form3();
+            f3.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Visible = true;
+            button1.Enabled=true;
+            button1.Text = "Начать тест";
+            button2.Visible = false;
+           
+            for (int i = 0; i < 10; i++)
+                rand[i] = 0;
+            d = 1;
+            t = 0;
+            h = 0;
+            g = 0;
+            label1.Text = "";
+            
+        }
+    }
+}
